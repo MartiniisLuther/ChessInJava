3cde0d6212ba787e43222373ef1643a88651a454
Martin Lutaaya <lutaayamart10@gmail.com>
2025-11-19 22:34:46 +0100

    Bishop piece class to manage mov't of the chess piece.

---
+++ src/chess/core/Bishop.java
@@ -0,0 +1,17 @@
+package chess.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a Bishop chess piece.
+ * 
+ * Bishops move diagonally any number of squares until blocked.
+ * They can't jump over pieces.
+ * 
+ * This 
+ */
+
+public class Bishop {
+    
+}
